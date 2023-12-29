ffb47a076d100b14f8fa8c0d194aa02ae4018c46
amirsahra <amirhosein.sahra@gmail.com>
2023-12-29 12:19:36 +0330

    feat(task): task page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import Header from "@/components/Header.vue";
+import TasksPage from "@/pages/TasksPage.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
   <Header />
-
+  <TasksPage />
+  <Footer />
 </template>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer class="py-5 text-center text-body-secondary bg-body-tertiary">
+    <p>Todo task by <a href="https://github.com/amirsahra/todo-task-front">amirsahra</a>.
+    </p>
+    <p class="mb-0">
+      <a href="#">Back to top</a>
+    </p>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>

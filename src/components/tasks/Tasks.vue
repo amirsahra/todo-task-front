98f90c91e8dd7f7a6fab0b4f591dfd9607243dd6
amirsahra <amirhosein.sahra@gmail.com>
2023-12-29 14:08:28 +0330

    feat(tasks): task completed and uncompleted

---
+++ src/components/tasks/Tasks.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Task from "@/components/tasks/Task.vue";
+
+defineProps({
+  tasks: Array
+})
+
+</script>
+
+<template>
+  <div class="card mt-2" v-if="tasks.length" >
+    <ul class="list-group list-group-flush">
+      <Task  v-for="task in tasks" :task="task" :key="task.id" />
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
